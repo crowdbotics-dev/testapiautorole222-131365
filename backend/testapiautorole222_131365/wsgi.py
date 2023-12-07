0bdefd32932294f7900cfd0cf619dca11104dcd8
Crowdbotics <engineering@crowdbotics.com>
2023-12-07 00:19:29 +0000

    Initial commit

---
+++ backend/testapiautorole222_131365/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for testapiautorole222_131365 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "testapiautorole222_131365.settings"
+)
+
+application = get_wsgi_application()
